1cbb677fb8cfca27adaacc0c7a82d6a049ec5b2d
SimonShepherd <simon.shepherd@lumary.com.au>
2021-12-21 12:08:38 +1030

    Add demo dataset

--- datasets/sample.sql
+++ datasets/sample.sql
@@ -5,8 +5,10 @@ CREATE TABLE "Account" (
 	"ParentId" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "Account" VALUES(1,'Simon Shepherd','');
-INSERT INTO "Account" VALUES(2,'Sample Account for Entitlements','');
+INSERT INTO "Account" VALUES(1,'Sample Account for Entitlements','');
+INSERT INTO "Account" VALUES(2,'Simon Shepherd','');
+INSERT INTO "Account" VALUES(3,'Simon Shepherd','');
+INSERT INTO "Account" VALUES(4,'Sample Account for Entitlements','');
 CREATE TABLE "Delivery_Item__c" (
 	id INTEGER NOT NULL, 
 	"Food_Expiration_Date__c" VARCHAR(255), 
@@ -15,6 +17,9 @@ CREATE TABLE "Delivery_Item__c" (
 	"Delivery__c" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
+INSERT INTO "Delivery_Item__c" VALUES(1,'2022-01-07','Non-refrigerated','Tim Tam','1');
+INSERT INTO "Delivery_Item__c" VALUES(2,'2025-12-12','Frozen','Hotdog','1');
+INSERT INTO "Delivery_Item__c" VALUES(3,'','Non-refrigerated','PopCorn','1');
 CREATE TABLE "Delivery__c" (
 	id INTEGER NOT NULL, 
 	"Scheduled_Date__c" VARCHAR(255), 
@@ -22,4 +27,5 @@ CREATE TABLE "Delivery__c" (
 	"Supplier__c" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
+INSERT INTO "Delivery__c" VALUES(1,'2021-12-21T01:00:00.000+0000','Requested','');
 COMMIT;
